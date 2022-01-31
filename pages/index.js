8812061145dc72d4a28e044256952f1a8416a99e
Brunomml <brunomyguel.miranda1988@gmail.com>
2022-01-31 14:59:37 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -4,11 +4,11 @@ function Home(){
     return (
         <div>
             <h1>Home</h1>
-            <Link href="/Sobre">
+            <Link href="/sobre">
                 <a>ir para a pagina sobre</a>
             </Link>
             <br />
-            <Link href="/Tempo">
+            <Link href="/tempo">
                 <a>ir para a pagina do tempo</a>
             </Link>
         </div>
