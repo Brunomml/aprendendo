5241dd98b1d41420c8baedb8cb03ccd70652613f
Brunomml <brunomyguel.miranda1988@gmail.com>
2022-01-31 16:29:20 -0300

    a

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,18 @@
-import Link from "next/link"
-
-function Home(){
+function Home(props){
     return (
         <div>
-            <h1>Home</h1>
-            <Link href="/sobre">
-                <a>ir para a pagina sobre</a>
-            </Link>
-            <br />
-            <Link href="/tempo">
-                <a>ir para a pagina do tempo</a>
-            </Link>
+            <div>{props.num}</div>
         </div>
     )
+}
+
+function getStaticProps() {
+    const num = 0
+    return {
+        props: {
+            num
+        }
+    }
 }
 
 export default Home
